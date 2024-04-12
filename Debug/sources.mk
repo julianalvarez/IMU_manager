63b622449240449053cff7a563c5473bc9515ed4
julianalvarez <jalvarez@plantium.com>
2024-04-12 10:33:56 -0300

    Modified include paths

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -14,20 +14,12 @@ OBJS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-board \
 canmsg \
-component/lists \
-component/serial_manager \
-component/uart \
-device \
 drivers/asm330lhh \
 drivers/asm330lhh/asm330lhh_STdC/driver \
-drivers \
 drivers/imu_adc8344 \
 drivers/lsm6dsm \
 drivers/lsm6dsm/lsm6dsm_STdC/driver \
 filter \
 source \
-utilities \
-xip \
 
